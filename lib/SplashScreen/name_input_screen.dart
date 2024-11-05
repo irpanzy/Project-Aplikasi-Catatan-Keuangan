ad7790856b23fb14ebad13ff9c1e47936e92cc5c
pasipasipada <irfanoputra27@gmail.com>
2024-11-06 00:59:46 +0700

    modify: file name input screen move to folder splash screen

---
+++ lib/SplashScreen/name_input_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NameInputScreen extends StatelessWidget {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime[400],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Siapa Nama Anda',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Masukkan nama anda',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              child: Text('Lanjut'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    Colors.black, 
+                foregroundColor:
+                    Colors.white, 
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                textStyle: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
